6b9883cf20779ae4f69b3806b75b6410b88c1ab2
AndreAS200320 <129907649+AndreAS200320@users.noreply.github.com>
2024-02-02 22:15:31 -0300

    commit Csharp

---
+++ ExemploAPI/ExemploAPI/Controllers/PessoaController.cs
@@ -0,0 +1,24 @@
+﻿using ExemploAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+
+        public Pessoa GetPessoa()
+        {
+            var vPessoa = new Pessoa();
+
+            vPessoa.Id = 1;
+            vPessoa.Nome = "José";
+            vPessoa.Telefone = "(18)988180248";
+            
+            return vPessoa;
+        }
+    }
+}
---
+++ ExemploAPI/ExemploAPI/Model/Pessoa.cs
@@ -0,0 +1,11 @@
+﻿namespace ExemploAPI.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set;}
+        public DateOnly DataNascimento { get; set; }
+
+    }
+}
